f8c571c576518e45ca4b118d9b2fb5412af00102
osama <om2951855@gmail.com>
2025-08-03 19:41:00 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to the Edu Platform</h1>
+      <p>Your one-stop solution for online learning.</p>
+    </main>
+  );
+}
