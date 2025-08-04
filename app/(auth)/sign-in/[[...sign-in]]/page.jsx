80df2f6161e790b69b39686ddc987c4354499347
osama <om2951855@gmail.com>
2025-08-05 01:07:38 +0300

    setup neon & drizzle orm

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -4,6 +4,7 @@ export default function Page() {
     return (
         <div className="flex items-center justify-center h-full w-full mt-20">
             <SignIn forceRedirectUrl="/dashboard" />
+
         </div>
     );
 }
