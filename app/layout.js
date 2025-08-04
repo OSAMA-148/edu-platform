c2179c63594cfbb791cc0ded9d81346b1a01302d
osama <om2951855@gmail.com>
2025-08-05 01:23:55 +0300

    add all components from shadcn

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,34 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "Edu Platform",
-  description: "Your one-stop solution for online learning.",
+    title: "Edu Platform",
+    description: "Your one-stop solution for online learning.",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className="scroll-smooth">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
         <ClerkProvider>
-          {children}
+            <html lang="en" className="scroll-smooth">
+                <body
+                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                >
+                    <Provider>{children}</Provider>
+                    <Toaster />
+                </body>
+            </html>
         </ClerkProvider>
-      </body>
-    </html>
-  );
+    );
 }
