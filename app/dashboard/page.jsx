11651f26d7897c958fb9103411d61411182f7f1d
osama <om2951855@gmail.com>
2025-08-04 19:27:52 +0300

    test

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,9 +2,7 @@
 const Dashboard = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-        <h1>Dashboard</h1>
-        <p>Welcome to your dashboard</p>
-        <p>This is a protected route</p>
+
 
 
 
