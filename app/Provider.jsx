baa60bb1278bd031c9e18319cebf9900f859b67f
osama <om2951855@gmail.com>
2025-08-05 01:54:36 +0300

    Send User Info To Database

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+"use client";
+import React, { useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import { UserDetailConext } from "@/context/UserContext";
+// import { SelectedChaptersConext } from "@/context/SelectedChapter";
 function Provider({ children }) {
-  return (
-    <div>
-      {children}
-    </div>
-  )
+    const { user } = useUser();
+
+    const [userDetail, setUserDetail] = useState();
+    // const [selectedChapters, setSelectedChapters] = useState(0);
+    useEffect(() => {
+        user && createNewUser();
+    }, [user]);
+
+    const createNewUser = async () => {
+        const result = await axios.post("/api/user", {
+            name: user?.fullName,
+            email: user?.primaryEmailAddress?.emailAddress,
+        });
+
+        console.log(result.data);
+    };
+    return (
+        <UserDetailConext.Provider value={{ userDetail, setUserDetail }}>
+            
+                <div>{children}</div>
+        </UserDetailConext.Provider>
+    );
 }
 
-export default Provider
+export default Provider;
